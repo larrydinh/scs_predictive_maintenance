4ae6f85d97cb1daa1c93849c7094a62a96faa9c4
Sharma, Amit <asharma@agtinternational.com>
2022-01-08 21:07:25 +0100

    chore: Add rest endpoint for getting machine-model information

--- source/packages/scs-pm-server/src/api.ts
+++ source/packages/scs-pm-server/src/api.ts
@@ -1,5 +1,8 @@
 import { Request, Response, Router } from 'express'
+import * as path from 'path'
+import { config } from './config'
 import { getVersionInformation, verifySystem } from './system'
+import { getDirectoryPath, log, readFile } from './utils'
 
 export const api = Router()
 
@@ -13,5 +16,25 @@ function systemInformation(_req: Request, res: Response) {
   res.status(200).json({ versionInfo })
 }
 
+function machinesModelInformation(_req: Request, res: Response) {
+  try {
+    const machinesModelInfoPath = path.join(
+      getDirectoryPath(
+        path.join(config.app.homeDir, config.app.appDirectory),
+        config.app.machinesDirectory,
+      ),
+    )
+    const machinesModelInfo = readFile(machinesModelInfoPath, config.app.machinesFileName)
+    res.status(200).json({ machinesModelInfo })
+  } catch (err) {
+    const errorMessage = `Unable to fetch the machines model information due: ${
+      (err as Error).message
+    }`
+    log.error(errorMessage)
+    res.status(500).json({ error: errorMessage })
+  }
+}
+
 api.get('/verify', verifyServerSetup)
 api.get('/version', systemInformation)
+api.get('/machines', machinesModelInformation)
