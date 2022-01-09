18ad60904bfb27d24555ee8819d4b683431af980
Sharma, Amit <asharma@agtinternational.com>
2022-01-09 21:20:00 +0100

    feat: Adding antd UI framework, several pages needed for the app e.g. dashboard, resource management are introduced

---
+++ source/packages/scs-pm-client/src/pages/dash-board-page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const DashBoardPage: React.FC = () => {
+  return <p>DashBoard Page</p>
+}
---
+++ source/packages/scs-pm-client/src/pages/resource-management-page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const ResourceManagementPage: React.FC = () => {
+  return <p>Resource Management Page</p>
+}
