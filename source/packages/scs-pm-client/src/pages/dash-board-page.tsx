91a001c772c58d6143ada039c32685de66911d40
Sharma, Amit <asharma@agtinternational.com>
2022-01-10 11:29:29 +0100

    feat: Update the page title, added the app icon

--- source/packages/scs-pm-client/src/pages/dash-board-page.tsx
+++ source/packages/scs-pm-client/src/pages/dash-board-page.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { Layout } from '../components'
 import { AppNavigation } from './app-navigation/app-navigation'
+import './dash-board-page.css'
 
 export const DashBoardPage: React.FC = () => {
   return (
     <Layout name="Dashboard" appNavigation={<AppNavigation />}>
-      <p>Dashboard</p>
+      <>
+        <div className="control-panel-container">
+          <div className="control-panel">
+            <p>Dashboard</p>
+          </div>
+          <img
+            src={`${process.env.PUBLIC_URL}/ic_scs.png`}
+            alt={`${process.env.PUBLIC_URL}/ic_scs.png`}
+            style={{
+              minWidth: '25%',
+              maxHeight: '100%',
+              display: 'inline-block',
+              margin: '0 auto',
+              marginTop: 50,
+            }}
+          />
+        </div>
+      </>
     </Layout>
   )
 }
