bf6e9cf3a23c96a3e48cb71c938dd39c9527eaa6
Sharma, Amit <asharma@agtinternational.com>
2022-01-30 21:57:35 +0100

    feat: Integrate the working of prediction model

--- source/packages/scs-pm-server/src/python-server/app.py
+++ source/packages/scs-pm-server/src/python-server/app.py
@@ -1,7 +1,7 @@
 import logging
-import pickle
 
 import joblib
+import pandas as pd
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
@@ -14,16 +14,17 @@ def home():
     modelPath = request.args.get('modelPath')
     app.logger.info('Received machine id is %s', incomingMachineId)
     app.logger.info('Model path is %s', modelPath)
-    # model_object = pickle.load(open(modelPath, "rb"))
-    # speed_desired_max,speed_avg,temperature_avg,temperature_max,pressure_avg,pressure_max,temperature_avg_avg,temperature_max_avg,pressure_avg_avg,pressure_max_avg
 
     modelObj = joblib.load(modelPath)
-    output1 = modelObj.predict([[1000.0,969.25,153.72,153.97,922.06,1117.25,152.56,153.35,854.19,1131.36]])
-    app.logger.info('End Model Object....', output1)
-    # object_file = pickle.load(modelPath, "rb")
-    # app.logger.info('Model path is %s', favorite_color)
 
-    return jsonify(machineID=incomingMachineId, modelPath=modelPath)
+    column_names = ["speed_desired_max","speed_avg","temperature_avg","temperature_max","pressure_avg","pressure_max","temperature_avg_avg","temperature_max_avg","pressure_avg_avg","pressure_max_avg"]
+    data = [[1000.0,1162.84,187.61,214.81,497.7,615.16,169.8,176.17,723.8,842.19]]
+    df = pd.DataFrame(data=data, columns = column_names)
+
+    modelPrediction = modelObj.predict(df)
+    app.logger.info('Model prediction is: %s', modelPrediction)
+
+    return jsonify(machineID=incomingMachineId, modelPath=modelPath, output=modelPrediction[0])
 
 if __name__ == "__main__":
     app.run(debug=True)
