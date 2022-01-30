ceb3b594cd74193a47d87e50a2a908735e486222
Sharma, Amit <asharma@agtinternational.com>
2022-01-30 16:54:36 +0100

    feat: Loading the pkl file using joblib rather than pickle

--- source/packages/scs-pm-server/src/python-server/app.py
+++ source/packages/scs-pm-server/src/python-server/app.py
@@ -1,6 +1,7 @@
 import logging
 import pickle
 
+import joblib
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
@@ -13,9 +14,12 @@ def home():
     modelPath = request.args.get('modelPath')
     app.logger.info('Received machine id is %s', incomingMachineId)
     app.logger.info('Model path is %s', modelPath)
+    # model_object = pickle.load(open(modelPath, "rb"))
+    # speed_desired_max,speed_avg,temperature_avg,temperature_max,pressure_avg,pressure_max,temperature_avg_avg,temperature_max_avg,pressure_avg_avg,pressure_max_avg
 
-    model_object = pickle.load( open(modelPath, "rb"))
-
+    modelObj = joblib.load(modelPath)
+    output1 = modelObj.predict([[1000.0,969.25,153.72,153.97,922.06,1117.25,152.56,153.35,854.19,1131.36]])
+    app.logger.info('End Model Object....', output1)
     # object_file = pickle.load(modelPath, "rb")
     # app.logger.info('Model path is %s', favorite_color)
 
